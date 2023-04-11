9ecdcc875154f2225beaff772a0e4098614c52d2
mazzlookman <mohamadlukmanaqib@gmail.com>
2023-04-12 04:13:28 +0700

    session_repository_impl :code

--- repository/session_repository.go
+++ repository/session_repository.go
@@ -1,1 +1,9 @@
 package repository
+
+import "login-management-go/model/domain"
+
+type SessionRepository interface {
+	Save(session domain.Session) domain.Session
+	FindByID(sessionID int) domain.Session
+	DeleteByID(sessionID int)
+}
--- repository/session_repository_impl.go
+++ repository/session_repository_impl.go
@@ -1,1 +1,35 @@
 package repository
+
+import (
+	"gorm.io/gorm"
+	"login-management-go/helper"
+	"login-management-go/model/domain"
+)
+
+type SessionRepositoryImpl struct {
+	db *gorm.DB
+}
+
+func (r *SessionRepositoryImpl) Save(session domain.Session) domain.Session {
+	err := r.db.Create(&session).Error
+	helper.PanicIfError(err)
+
+	return session
+}
+
+func (r *SessionRepositoryImpl) FindByID(sessionID int) domain.Session {
+	session := domain.Session{}
+	err := r.db.Find(&session, "id=?", sessionID).Error
+	helper.PanicIfError(err)
+
+	return session
+}
+
+func (r *SessionRepositoryImpl) DeleteByID(sessionID int) {
+	err := r.db.Delete(&domain.Session{}, "id=?", sessionID).Error
+	helper.PanicIfError(err)
+}
+
+func NewSessionRepository(db *gorm.DB) SessionRepository {
+	return &SessionRepositoryImpl{db: db}
+}
