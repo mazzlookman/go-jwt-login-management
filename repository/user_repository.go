0bf08d31c43c552198a7bcf3b01e371e673d5621
mazzlookman <mohamadlukmanaqib@gmail.com>
2023-04-12 02:41:11 +0700

    user repository impl

---
+++ repository/user_repository.go
@@ -0,0 +1,10 @@
+package repository
+
+import "login-management-go/model/domain"
+
+type UserRepository interface {
+	Save(user domain.User) domain.User
+	Update(user domain.User) domain.User
+	FindByID(userID int) domain.User
+	DeleteByID(userID int)
+}
---
+++ repository/user_repository_impl.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"gorm.io/gorm"
+	"login-management-go/helper"
+	"login-management-go/model/domain"
+)
+
+type UserRepositoryImpl struct {
+	db *gorm.DB
+}
+
+func (r *UserRepositoryImpl) Save(user domain.User) domain.User {
+	err := r.db.Create(&user).Error
+	helper.PanicIfError(err)
+
+	return user
+}
+
+func (r *UserRepositoryImpl) Update(user domain.User) domain.User {
+	err := r.db.Save(&user).Error
+	helper.PanicIfError(err)
+
+	return user
+}
+
+func (r *UserRepositoryImpl) FindByID(userID int) domain.User {
+	user := domain.User{}
+	err := r.db.Where("id=?", userID).Find(&user).Error
+	helper.PanicIfError(err)
+
+	return user
+}
+
+func (r *UserRepositoryImpl) DeleteByID(userID int) {
+	user := domain.User{}
+	err := r.db.Delete(&user, userID).Error
+	helper.PanicIfError(err)
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &UserRepositoryImpl{db: db}
+}
